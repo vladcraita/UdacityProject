dc9b7f9485abe1e957e2d6942335d9cbd2f70117
Vlad Craita <vlad.craita@yahoo.com>
2022-12-07 19:24:13 +0200

    review changes

--- main.go
+++ main.go
@@ -37,9 +37,16 @@ func getCustomers(w http.ResponseWriter, r *http.Request) {
 
 func getCustomer(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
+
 	id, _ := strconv.ParseUint(mux.Vars(r)["id"], 10, 64)
-	json.NewEncoder(w).Encode(db[id])
+
+	if customer, ok := db[id]; ok {
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(customer)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode(getErrorMessage("No customer found with id " + strconv.FormatUint(id, 10)))
+	}
 }
 
 func addCustomer(w http.ResponseWriter, r *http.Request) {
@@ -54,12 +61,57 @@ func addCustomer(w http.ResponseWriter, r *http.Request) {
 
 	json.Unmarshal(reqBody, &newCustomer)
 
-	id := uint64(len(db)) + 1
-	newCustomer.ID = id
-	db[id] = newCustomer
+	if newCustomer.isValid() {
+		id := uint64(len(db)) + 1
+		newCustomer.ID = id
+		db[id] = newCustomer
 
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(newCustomer)
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(newCustomer)
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(getErrorMessage("Name and Email are mandatory fields"))
+	}
+}
+
+func updateCustomer(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var update Customer
+	reqBody, _ := io.ReadAll(r.Body)
+
+	valid := json.Valid(reqBody)
+	if !valid {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	json.Unmarshal(reqBody, &update)
+
+	id, _ := strconv.ParseUint(mux.Vars(r)["id"], 10, 64)
+
+	update.setId(id)
+
+	existingCustomer := db[id]
+	copier.CopyWithOption(&existingCustomer, &update, copier.Option{IgnoreEmpty: true, DeepCopy: true})
+
+	if existingCustomer.isValid() {
+		db[id] = existingCustomer
+
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(existingCustomer)
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(getErrorMessage("Name and Email are mandatory fields"))
+	}
+}
+
+func deleteCustomer(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusNoContent)
+	id, _ := strconv.ParseUint(mux.Vars(r)["id"], 10, 64)
+	delete(db, id)
+	json.NewEncoder(w).Encode(db)
 }
 
 func replaceCustomer(w http.ResponseWriter, r *http.Request) {
@@ -77,46 +129,40 @@ func replaceCustomer(w http.ResponseWriter, r *http.Request) {
 
 	json.Unmarshal(reqBody, &replaceCustomer)
 
-	id, _ := strconv.ParseUint(mux.Vars(r)["id"], 10, 64)
-	replaceCustomer.ID = id
-	db[id] = replaceCustomer
+	if replaceCustomer.isValid() {
+		id, _ := strconv.ParseUint(mux.Vars(r)["id"], 10, 64)
+		replaceCustomer.ID = id
+		db[id] = replaceCustomer
 
-	json.NewEncoder(w).Encode(replaceCustomer)
-}
-
-func updateCustomer(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-
-	var update Customer
-	reqBody, _ := io.ReadAll(r.Body)
-
-	valid := json.Valid(reqBody)
-	if !valid {
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(replaceCustomer)
+	} else {
 		w.WriteHeader(http.StatusBadRequest)
-		return
+		json.NewEncoder(w).Encode(getErrorMessage("Name and Email are mandatory fields for replacing a customer at id. If a partial update is desired use PATCH"))
 	}
-
-	json.Unmarshal(reqBody, &update)
-
-	id, _ := strconv.ParseUint(mux.Vars(r)["id"], 10, 64)
-
-	existingCustomer := db[id]
-	copier.CopyWithOption(&existingCustomer, &update, copier.Option{IgnoreEmpty: true, DeepCopy: true})
-	db[id] = existingCustomer
-	json.NewEncoder(w).Encode(existingCustomer)
 }
 
-func deleteCustomer(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusNoContent)
-	id, _ := strconv.ParseUint(mux.Vars(r)["id"], 10, 64)
-	delete(db, id)
-	json.NewEncoder(w).Encode(db)
+func (customer *Customer) setId(id uint64) {
+	customer.ID = id
+}
+
+func (customer *Customer) isValid() bool {
+	return len(customer.NAME) > 0 && len(customer.EMAIL) > 0
+}
+
+func showIndexPage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/index.html")
+}
+
+func getErrorMessage(message string) map[string]string {
+	return map[string]string{
+		"error": message,
+	}
 }
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", showIndexPage)
 	router.HandleFunc("/customers", getCustomers).Methods("GET")
 	router.HandleFunc("/customers", addCustomer).Methods("POST")
 	router.HandleFunc("/customers/{id}", getCustomer).Methods("GET")
